1d018ea0a091fa8455836559eb8bea8c95fb92df
fira swidan <firas@local.com>
2018-12-05 17:01:26 +0200

    : Add git pre-commit checks to other projects
    Adding files anew to repository after moving .git folder location as
    indicated in first reply of:
    https://stackoverflow.com/questions/2411031/how-do-i-clone-into-a-non-empty-directory
    Task-Url:

---
+++ src/main/java/com/geneway/alerts/injection/testing/TestAlertsModule.java
@@ -0,0 +1,36 @@
+package com.geneway.alerts.injection.testing;
+
+import java.util.Properties;
+
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import com.icegreen.greenmail.util.GreenMail;
+import com.icegreen.greenmail.util.ServerSetupTest;
+
+
+/**
+ * A testing helper for the alerts module. To override the {@code AlertsModule}
+ * behavior use:
+ * {@code Modules.override(new AlertsModule()).with(new TestAlertsModule())}
+ * @author firas1
+ *
+ */
+public class TestAlertsModule extends AbstractModule {
+	public static final GreenMail MAIL_SERVER = new GreenMail(ServerSetupTest.SMTP);
+	public static final String LOCALHOST = "127.0.0.1";
+
+	@Provides
+	protected Properties provideProperties() {
+		Properties properties = new Properties();
+		properties.put("mail.smtp.host", LOCALHOST);
+		properties.put("mail.smtp.auth", "true");
+		properties.put("mail.smtp.port", ServerSetupTest.SMTP.getPort());
+		return properties;
+	}
+
+	@Override
+	protected void configure() {
+		//Binding provided through provides methods
+	}
+}
